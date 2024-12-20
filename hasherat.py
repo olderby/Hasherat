4bb92acae265b428083f2a9f3af8c5dfb17e34e5
Oscar Derby <oscar.derby3@gmail.com>
2024-12-20 07:56:41 -0500

    Initial commit for Hasherat Project

---
+++ hasherat.py
@@ -0,0 +1,11 @@
+#get modules for hashing
+
+#ask user for password input
+
+#hash the password using a salt
+    #the salt is sensative and secret
+
+#store the hashed password
+    #the password is sensative and secret
+
+#validate a user's password by comparing it against the stored hash.
